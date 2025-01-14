1d50597e0e6db5d69bf094647ebd5951e3ae76c1
Adewunmi <shonibareadewunmicomfort@gmail.com>
2025-01-14 15:45:02 +0100

    add logic in member.rb and library.rb

--- library.rb
+++ library.rb
@@ -5,13 +5,22 @@ class Library
     @books = []
     @members = []
     end
+
+    def add_member(member)
+        @member << member
+    end
+
     def add_book(book)
+        @books << book
     # Add validation and book addition logic
     end
+
     def remove_book(book_id)
+        @books.delete(book_id)
     # Add book removal logic
     end
     def search_books(query)
+        @books.select { |book| book.query == query }
     # Implement search functionality
     end
 end
--- member.rb
+++ member.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class Member
     attr_accessor :name, :email, :membership_status
     attr_reader :id, :borrowed_books
@@ -8,10 +10,30 @@ class Member
     @membership_status = "active"
     @borrowed_books = []
     end
+
+    def check_membership(name, email)
+        if @name == name && @email == email
+            @membership_status
+        else
+            @membership_status = "in-active"
+        end    
+    end
+
     def borrow_book(book)
-    # Implement borrowing logic with validations
+        if @borrowed_books.include?(book)
+            puts "#{book.title} has been borrowed"
+        else
+            @borrowed_books << book
+            book.status = "borrowed"
+        end
     end
+
     def return_book(book)
-    # Implement return logic with validations
+        if @borrowed_books.include?(book)
+            @borrowed_books.delete(book)
+            book.status = "available"
+        else
+            puts "#{book.title} has not been borrowed"
+        end
     end
-   end
+end
