f42aa0e2a5a5fae36f08c009aa1aa6449b330d16
Adewunmi <shonibareadewunmicomfort@gmail.com>
2025-01-17 15:42:25 +0100

    add calculate due date logic

--- transaction.rb
+++ transaction.rb
@@ -1,5 +1,8 @@
 class Transaction
     attr_reader :id, :book, :member, :checkout_date, :due_date, :return_date
+
+    LATE_FEE = 50.00
+
     def initialize(book:, member:)
     @id = generate_unique_id
     @book = book
@@ -8,9 +11,15 @@ class Transaction
     @due_date = calculate_due_date
     @return_date = nil
     end
+
+    def calculate_late_fees
+        overdue = ((@return_date || Time.now) - @due_date).to_i/(60 * 60 * 24)
+    end
+
     private
+    
     def calculate_due_date
-    # Implement due date calculation
+        @checkout_date + (14 * 24 * 60 * 60)
     end
    end
    
